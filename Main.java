fefbc6fd8bdd161a4fb8322c45eaf63d013c647f
Conal <122840957+ConalHamilton@users.noreply.github.com>
2025-03-02 13:22:38 -0800

    Update Main.java
    Test Commit, No Code

--- Main.java
+++ Main.java
@@ -1,4 +1,20 @@
-// PARTNER NAME: 
-// PARTNER NAME: 
-// CS111 SECTION #: 
-// DATE:
+// PARTNER NAME: Conal Hamilton
+// CS111 SECTION #: 1112
+// DATE: 2 March 2025
+public class Main 
+{
+  public static void main(String[] args) 
+	{
+
+		/***** DECLARATION SECTION *****/
+
+		/***** INITIALIZATION SECTION *****/
+
+		/***** INPUT SECTION *****/
+		
+		/***** CALCULATION & PROCESSING SECTION *****/
+
+		/***** OUTPUT SECTION *****/
+
+  }
+}
