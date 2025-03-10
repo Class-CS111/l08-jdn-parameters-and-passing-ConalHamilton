731ee2525cae10658e3867df7de20c4ff2aab96a
Conal <122840957+ConalHamilton@users.noreply.github.com>
2025-03-09 18:24:43 -0700

    Update Main.java
    Created parameters to pass the method Katherine Johnson's birthday as a default date, commented out old tests

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 // PARTNER NAME: Conal Hamilton
 // CS111 SECTION #: 1112
-// DATE: 2 March 2025
+// DATE: Updated 9 March 2025
 public class Main 
 {
   public static void main(String[] args) 
 {
-	System.out.println(calcJulianDate(3,2,2025)); //Test for today's date, should return 2460737
-	System.out.println(calcJulianDate(8,26,1918)); //Test for Katherin Johnson's birthday, should return 2421832
-	System.out.println(calcJulianDate(11,7,1917)); //Test for another day, should return 2421540
+	int monthToday, dayToday, yearToday;
+	monthToday = 8;
+	dayToday = 26;
+	yearToday = 1918;
+	System.out.println(calcJulianDate(monthToday, dayToday, yearToday));
+	//System.out.println(calcJulianDate(3,2,2025)); //Test for today's date, should return 2460737
+	//System.out.println(calcJulianDate(8,26,1918)); //Test for Katherine Johnson's birthday, should return 2421832
+	//System.out.println(calcJulianDate(11,7,1917)); //Test for another day, should return 2421540
 }
   public static int calcJulianDate(int monthToday, int dayToday, int yearToday)
 {
@@ -17,4 +22,4 @@ public class Main
 	int julianDay = dayToday + (monthVar * 153 + 2) / 5 + yearVar * 365 + yearVar / 4 - yearVar / 100 + yearVar / 400 - 32045; //Formula taken from Lab 03
 	return julianDay; //Gives back JDN as an integer
 }
-	}
+}
